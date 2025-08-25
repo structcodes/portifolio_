b4d5b793af703eee6975377ad80aed497fa8722f
tainasts <tainaroberta102@gmail.com>
2025-08-25 13:07:04 -0300

    portifolio finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+// Menu Hamburguer
+const btnMenu = document.getElementById('btnMenu');
+ const navList = document.querySelector('.nav-menu ul');
+
+ btnMenu.addEventListener('click', () => {
+   navList.classList.toggle('active');
+ })
+
+// Efeito cortina
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+    document.querySelector(".cortina").style.display = "none";
+    document.querySelector("header").style.opacity = "1";
+    document.querySelector("main").style.opacity = "1";
+   }, 1500); // tempo da animação
+      });
